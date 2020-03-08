e1dac9ef059b71e9faa228332aa4a26e5d6f3617
xfhy <xfhy666@qq.com>
2020-03-08 15:10:47 +0800

    Provider 提供多个数据

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -28,9 +28,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //通过Provider组件封装数据资源
     //因Provider是InheritedWidget的语法糖,所以它是一个Widget
-    return ChangeNotifierProvider.value(
-      //需要共享的数据资源
-      value: CounterModel(),
+    //ChangeNotifierProvider只能搞一个
+    //MultiProvider可以搞多个
+    return MultiProvider(
+      providers: [
+        //注入字体大小  下个界面读出来
+        Provider.value(value: 30.0),
+        //注入计数器实例
+        ChangeNotifierProvider.value(value: CounterModel())
+      ],
       child: MaterialApp(
         home: FirstPage(),
       ),
@@ -43,16 +49,34 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //取出资源  类型是CounterModel
-    CounterModel _counter = Provider.of<CounterModel>(context);
+    //获取计时器实例
+    final _counter = Provider.of<CounterModel>(context);
+    //获取字体大小
+    final textSize = Provider.of<double>(context);
+
+    /*
+    *
+//使用Consumer2获取两个数据资源
+Consumer2<CounterModel,double>(
+  //builder函数以参数的形式提供了数据资源
+  builder: (context, CounterModel counter, double textSize, _) => Text(
+      'Value: ${counter.counter}',
+      style: TextStyle(fontSize: textSize))
+)
+* 我们最多可以使用到 Consumer6，即共享 6 个数据资源。
+    * */
+
     return Scaffold(
       body: Center(
-        child: Text('Counter: ${_counter.counter}'),
+        child: Text(
+          'Counter: ${_counter.counter}',
+          style: TextStyle(fontSize: textSize),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Text('Go'),
-        onPressed: () =>
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => SecondPage())),
+        onPressed: () => Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => SecondPage())),
       ),
     );
   }
@@ -70,17 +94,15 @@ class SecondPage extends StatelessWidget {
       body: Consumer(
         //builder函数可以直接获取到counter参数
         //Consumer 中的 builder 实际上就是真正刷新 UI 的函数，它接收 3 个参数，即 context、model 和 child
-        builder: (context, CounterModel counter, _) =>
-            Center(
-                child: Text('Value: ${counter.counter}'),
-            ),
+        builder: (context, CounterModel counter, _) => Center(
+          child: Text('Value: ${counter.counter}'),
+        ),
       ),
       floatingActionButton: Consumer<CounterModel>(
-        builder: (context, CounterModel counter, child) =>
-            FloatingActionButton(
-              onPressed: counter.increment,
-              child: child,
-            ),
+        builder: (context, CounterModel counter, child) => FloatingActionButton(
+          onPressed: counter.increment,
+          child: child,
+        ),
         child: Icon(Icons.add),
       ),
     );
